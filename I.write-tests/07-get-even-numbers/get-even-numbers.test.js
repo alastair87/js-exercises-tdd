1b6480e8941eb66e361c55c24bebe3b960afa943
Alastair Cooper <alastair.cooper@outlook.com>
2018-04-14 16:59:02 +0100

    I - 07

--- I.write-tests/07-get-even-numbers/get-even-numbers.test.js
+++ I.write-tests/07-get-even-numbers/get-even-numbers.test.js
@@ -1,3 +1,12 @@
 // example
 // input: [22, 13, 73, 82, 4];
 // expected: [22, 82, 4];
+
+var getEven = require("./get-even-numbers");
+
+test("check that only even numbers are returned", () => {
+  var input = [22, 13, 73, 82, 4];
+  var expected = [22, 82, 4];
+  var output = getEven(input);
+  expect(output).toEqual(expected);
+});

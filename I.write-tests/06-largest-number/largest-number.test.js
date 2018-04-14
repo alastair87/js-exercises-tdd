ae17ea9dd42fc91dd2c12660c0b1b9883e3d5956
Alastair Cooper <alastair.cooper@outlook.com>
2018-04-14 16:54:08 +0100

    I - 06

--- I.write-tests/06-largest-number/largest-number.test.js
+++ I.write-tests/06-largest-number/largest-number.test.js
@@ -3,3 +3,15 @@
 // expected: 88;
 
 // also test that the original array hasn't changed
+
+var getLargestNumber = require("./largest-number");
+
+test("number is largest and input array is unchanged", () => {
+  var input = [3, 21, 88, 4, 36];
+  var expected = 88;
+  var inputCopy = input.slice();
+  var output = getLargestNumber(input);
+
+  expect(output).toEqual(expected);
+  expect(input).toEqual(inputCopy);
+});

9daa41775ad8577c135eb21f0cf2f5c7e49fec03
Alastair Cooper <alastair.cooper@outlook.com>
2018-04-14 16:44:43 +0100

    I - 05

--- I.write-tests/05-get-second-third-smallest/get-second-third.test.js
+++ I.write-tests/05-get-second-third-smallest/get-second-third.test.js
@@ -3,3 +3,15 @@
 // expected output = [6, 8];
 
 // also test that the original array has not changed
+
+var getSecondThird = require("./get-second-third");
+
+test("returns 2nd and 3rd largest number and that array is unchanged", () => {
+  var input = [90, 5, 11, 8, 6];
+  var inputCopy = input.slice();
+  var expected = [6, 8];
+  var output = getSecondThird(input);
+
+  expect(output).toEqual(expected);
+  expect(input).toEqual(inputCopy);
+});

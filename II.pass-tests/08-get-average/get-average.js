87fa0eda1515c40477c341ebd3e688b02d041376
Alastair Cooper <alastair87@users.noreply.github.com>
2018-04-19 15:23:25 +0100

    completed get-average

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,17 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+module.exports = average;
+
+function average(array) {
+  var sum = 0,
+    count = 0;
+  array.forEach(element => {
+    if (typeof element == "number") {
+      sum += element;
+      count++;
+    }
+  });
+  return sum / count;
+}

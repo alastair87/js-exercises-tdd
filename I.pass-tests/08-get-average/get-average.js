8f6ec3bcc36ce35f64b34095ac0e94d63c8f2c40
Alastair Cooper <alastair.cooper@outlook.com>
2019-05-03 18:25:06 +0100

    passing get-average

--- I.pass-tests/08-get-average/get-average.js
+++ I.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,10 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+const average = elements => {
+  let numbers = elements.filter(element => typeof element === "number");
+  return numbers.reduce((a, b) => a + b, 0) / numbers.length;
+};
+
+module.exports = average;

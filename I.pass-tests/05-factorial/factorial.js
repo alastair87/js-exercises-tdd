1b8ccc294cabb2a5289284207c4669799c19ed22
Alastair Cooper <alastair.cooper@outlook.com>
2019-05-03 09:58:25 +0100

    factorial passing

--- I.pass-tests/05-factorial/factorial.js
+++ I.pass-tests/05-factorial/factorial.js
@@ -8,6 +8,12 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+  let value = 1;
+  for (let i = int; i > 1; i--) {
+    value = value * i;
+  }
+  return value;
+}
 
 module.exports = factorial;

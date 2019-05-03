528938e71ad4bf2f0eabbdb8d751d243fc085321
Alastair Cooper <alastair.cooper@outlook.com>
2019-05-03 19:34:36 +0100

    completed greet-people

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,11 @@
+const greetPeople = require("./greet-people");
+
 test("print list of names prefixed with Hello", function() {
   // Arrange
+  const names = ["Alastair", "Irina", "Etza"];
+  const expected = "Hello AlastairIrinaEtza";
   // Act
+  const result = greetPeople(names);
   // Assert
+  expect(result).toEqual(expected);
 });

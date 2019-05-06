806e01ef2eae68b76d5b5bb1685d76d61e241a6f
Alastair Cooper <alastair.cooper@outlook.com>
2019-05-06 19:31:04 +0100

    completed remove-vowels-in-array

--- II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.js
+++ II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.js
@@ -1,13 +1,19 @@
 var removeVowels = require("../02-remove-vowels/remove-vowels");
 
 function removeVowelsForWords(words) {
-  var result = words.map(function(word) {
+  let result = words.map(function(word) {
     return removeVowels(word);
   });
 
+  result = result.map(removeUnderscores);
   return result;
 }
 
+const removeUnderscores = word => {
+  var result = [], characters = word.split("");
+  characters.forEach(character => {if (character !== "_") result.push(character);})
+  return result.join(""); 
+}
 module.exports = removeVowelsForWords;
 
 /*
--- II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ II.write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -1,7 +1,12 @@
+const removeVowelsFromWords = require('./remove-vowels-in-array');
+
 test("remove vowels from all words in array", function() {
   // Arrange
+  const input = ["Irina", "Etza", "Daniel"], expected = ["rn", "tz", "Dnl"];
   // Act
+  const result = removeVowelsFromWords(input);
   // Assert
+  expect(result).toEqual(expected);
 });
 
 // example
